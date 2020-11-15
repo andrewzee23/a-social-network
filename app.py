221ebd6251800baf73f91dc5018985070de32955
andrewzee23 <andrewazamora@gmail.com>
2020-11-15 09:51:14 -0600

    cr8 new routes for reg/login and htmls, forms.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, url_for, render_template
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '2fad37ea88d279117a11d079689e93e9'
 # dummy data
 posts = [
     {
@@ -29,6 +31,15 @@ def home():
 def about():
     return render_template('about.html', title = 'About')
 
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title = 'Register', form= form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Login', form= form)
 
 if __name__ == '__main__':
     app.run(debug=True)
