241fb790b8938aaace31d2aeeaa407a711665e16
andrewzee23 <andrewazamora@gmail.com>
2020-11-16 10:55:38 -0600

    created username/email validation

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, validators, ValidationError
 from wtforms.validators import DataRequired, Length, Email, email_validator, EqualTo
 import email_validator
+from app.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',validators = [DataRequired(), Length(min=2, max=20)])
@@ -13,6 +14,19 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
 
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose another one.')
+
+
+    def validate_email(self, email):
+
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose another one.')
 
 
 
