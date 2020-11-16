ad472d390487c9af37b59d868af94b5388c4a1bc
andrewzee23 <andrewazamora@gmail.com>
2020-11-16 10:32:24 -0600

    imported bcrypt

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2fad37ea88d279117a11d079689e93e9'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
 
 from app import routes
