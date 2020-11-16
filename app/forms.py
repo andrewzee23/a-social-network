de93732c3f133b019d11f3ae98ccc7b56d056c24
andrewzee23 <andrewazamora@gmail.com>
2020-11-16 12:18:20 -0600

    updated form dependencies

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, validators, ValidationError
 from wtforms.validators import DataRequired, Length, Email, email_validator, EqualTo
