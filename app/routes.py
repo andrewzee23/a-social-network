4a9115dbd84b1981a56a6a79fdee46f42f71e99b
andrewzee23 <andrewazamora@gmail.com>
2020-11-16 11:36:54 -0600

    login completed

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import url_for, render_template, flash, redirect
+from flask import url_for, render_template, flash, redirect, request
 from app import app, db, bcrypt
 from app.forms import RegistrationForm, LoginForm
 from app.models import User, Post
@@ -64,7 +64,10 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
 
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+            
         else:
 
             flash('Login Unsuccessful. Please check email and password', 'danger')
