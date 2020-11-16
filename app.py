141e8b7431b92f71ad6bf586cdc934e5e20aff46
andrewzee23 <andrewazamora@gmail.com>
2020-11-16 09:43:27 -0600

    creaated db and minor format to app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
-        return f'User('{self.username}', '{self.email}','{self.image_file}')'
+        return f'User("{self.username}", "{self.email}","{self.image_file}")'
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -30,7 +30,7 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return f'User('{self.title}', '{self.date_posted}')'
+        return f'User("{self.title}", "{self.date_posted}")'
 
 # dummy data
 
