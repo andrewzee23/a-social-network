d6fa4a50009faed8e97308fa81f6c6299942d502
andrewzee23 <andrewazamora@gmail.com>
2020-11-16 12:49:51 -0600

    resizing of profile_pics

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+from PIL import Image
 from flask import url_for, render_template, flash, redirect, request
 from app import app, db, bcrypt
 from app.forms import RegistrationForm, LoginForm, UpdateAccountForm
@@ -88,7 +89,11 @@ def save_picture(form_picture):
     _ , f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-    form_picture.save(picture_path)
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.tumbnail(output_size)
+
+    i.save(picture_path)
 
     return picture_fn
 
