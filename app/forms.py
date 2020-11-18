5e7962509d9aaa9355b2973dcb26d1c5d858fe83
andrewzee23 <andrewazamora@gmail.com>
2020-11-17 19:27:43 -0600

    created PostForm forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, validators, ValidationError
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, validators, ValidationError, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, email_validator, EqualTo
 import email_validator
 from app.models import User
@@ -65,3 +65,11 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('That email is taken. Please choose another one.')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+
+    content = TextAreaField('Content', validators=[DataRequired()])
+
+    submit = SubmitField('Post')
