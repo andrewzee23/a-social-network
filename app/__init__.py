62af93d2da32e5ea05363834ef1365be74206cef
andrewzee23 <andrewazamora@gmail.com>
2020-11-18 10:26:01 -0600

    setup email __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from config import username, password
 
 
 app = Flask(__name__)
@@ -12,6 +14,11 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = username
+app.config['MAIL_PASSWORD'] = password
+mail = Mail(app)
 
 from app import routes
