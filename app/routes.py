5f62f00b25fff97356a827ddf1c588dd6ea4bee0
andrewzee23 <andrewazamora@gmail.com>
2020-11-17 19:24:39 -0600

    created new post route

--- app/routes.py
+++ app/routes.py
@@ -127,3 +127,9 @@ def account():
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
 
     return render_template('account.html', title = 'Account', image_file = image_file, form = form)
+
+@app.route('/post/new', methods = ['GET', 'POST'])
+@login_required
+def new_post():
+
+    render_template('create_post.html', title = 'New Post')
