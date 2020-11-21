fa4b7db253e02c1852c64f6954d9854ff001f697
andrewzee23 <andrewazamora@gmail.com>
2020-11-20 20:32:23 -0600

    created create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,7 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
+
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -17,10 +16,15 @@ login_manager.login_message_category = 'info'
 
 mail = Mail(app)
 
-from app.users.routes import users
-from app.posts.routes import posts
-from app.main.routes import main
 
-app.register_blueprint(users)
-app.register_blueprint(posts)
-app.register_blueprint(main)
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    from app.users.routes import users
+    from app.posts.routes import posts
+    from app.main.routes import main
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
