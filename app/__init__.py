bbdc25f13d3d7dffb698bee9b183c889654d89b0
andrewzee23 <andrewazamora@gmail.com>
2020-11-20 20:29:08 -0600

    minor change

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-
+from app.config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
