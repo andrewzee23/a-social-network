bcc44327c2383a4a86b4faeafdd4c361c33fca9e
andrewzee23 <andrewazamora@gmail.com>
2020-11-20 19:22:39 -0600

    changed 'app.route' to 'posts.route'

--- app/posts/routes.py
+++ app/posts/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 
 posts = Blueprint('posts', __name__)
 
-@app.route('/post/new', methods = ['GET', 'POST'])
+@posts.route('/post/new', methods = ['GET', 'POST'])
 @login_required
 def new_post():
 
@@ -19,14 +19,14 @@ def new_post():
     return render_template('create_post.html', title = 'New Post', form = form, legend = 'New Post')
 
 
-@app.route('/post/<int:post_id>', methods = ['GET', 'POST'])
+@posts.route('/post/<int:post_id>', methods = ['GET', 'POST'])
 def post(post_id):
 
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title = post.title, post=post)
 
 
-@app.route('/post/<int:post_id>/update', methods = ['GET', 'POST'])
+@posts.route('/post/<int:post_id>/update', methods = ['GET', 'POST'])
 @login_required
 def update_post(post_id):
     
@@ -47,7 +47,7 @@ def update_post(post_id):
     return render_template('create_post.html', title = 'Update Post', form = form, legend = 'Update Post')
 
 
-@app.route('/post/<int:post_id>/delete', methods = ['POST'])
+@posts.route('/post/<int:post_id>/delete', methods = ['POST'])
 @login_required
 def delete_post(post_id):
 
