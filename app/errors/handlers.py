38fefd15949664e3c8bce078c25bb5fb95011f04
andrewzee23 <andrewazamora@gmail.com>
2020-11-24 10:36:30 -0600

    404 errorhandler

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,3 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 errors = Blueprint('errors', __name__)
+
+
+@errors.app_errorhandler(404)
+def error_404(error):
+    return render_template('errors/404.html'), 404
+    
